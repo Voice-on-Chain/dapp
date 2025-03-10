1077490dbac41f7d6cf206c7744805e00c876c05
Brad <klausmickelson52@gmail.com>
2025-03-10 22:44:37 +0100

    feat: disable WalletConnectButton while connecting

--- src/components/shared/WalletConnectButton.tsx
+++ src/components/shared/WalletConnectButton.tsx
@@ -4,9 +4,10 @@ import { useConnect } from "wagmi";
 import { metaMask } from "wagmi/connectors";
 
 const WalletConnectButton = () => {
-  const { connect } = useConnect();
+  const { connect, isPending } = useConnect();
   return (
     <button
+      disabled={isPending}
       onClick={() => connect({ connector: metaMask() })}
       className="flex items-center text-xs lg:text-sm font-medium text-accent space-x-2 border border-athens rounded-lg px-4 py-[0.625rem]"
     >
